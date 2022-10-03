1b0af8b70d918aefbce8f94ed8ea33a6b4e3d5c8
VladRudenko <j0019285@jmail.com>
2022-10-03 22:16:39 +0300

    Has been added possibility to watch geolocation on the map

--- main.py
+++ main.py
@@ -1,13 +1,41 @@
 import requests
+import folium
+from pyfiglet import Figlet
 
 
 def get_info_by_ip(ip):
     try:
         response = requests.get(url=f"http://ip-api.com/json/{ip}").json()
-        print(response)
+
+        data = {
+            "IP": response.get("query"),
+            "COUNTRY": response.get("country"),
+            "COUNTRY_CODE": response.get("countryCode"),
+            "REGION_NAME": response.get("regionName"),
+            "CITY": response.get("city"),
+            "ZIP_POSTAL_CODE": response.get("zip"),
+            "LAT": response.get("lat"),
+            "LON": response.get("lon"),
+            "TIMEZONE": response.get("timezone"),
+            "INERNET_PROVIDER": response.get("isp"),
+            "ORGANIZATION": response.get("org"),
+            "ASN": response.get("as"),
+        }
+
+        for key, value in data.items():
+            print(f"{key} >>> {value}")
+
+        area = folium.Map(location=[data["LAT"], data["LON"]], zoom_start=15)
+        folium.Marker(location=[data["LAT"], data["LON"]], icon=folium.Icon(
+            color="red", icon="glyphicon-record", iconColor="white")).add_to(area)
+        area.save(f'{data["CITY"]}_{data["IP"]}.html')
+
     except requests.exceptions.ConnectionError:
         print("Huston, we have got connection troubles!!")
 
 
+preview_text = Figlet(font="slant")
+print(preview_text.renderText("IP_INFO"))
 ip = input("Please, enter IP: ")
+print("-" * 30)
 get_info_by_ip(ip=ip)
